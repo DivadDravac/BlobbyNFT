3b2ffa417a0917b71b105644ac815b0b53904cad
Divad Dravac <david.cavard@gmail.com>
2022-05-30 19:28:44 +0200

    sdf

--- BlobWorker.js
+++ BlobWorker.js
@@ -1,37 +1,4 @@
 
-function readTextFile(file)
-{
-    var rawFile = new XMLHttpRequest();
-    var allText = "";
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-              allText = rawFile.responseText;
-            }
-        }
-    }
-    rawFile.send(null);
-    return allText;
-}
-
-//var img = new Image();
-var svg = new Blob([readTextFile("BlobbyThing.svg")], {type: "image/svg+xml"});
-var url = URL.createObjectURL(svg);
-
-//img.onload = function() {
-//  var canvas = document.getElementById("canvas");
-//  var ctx = canvas.getContext("2d");
-//  ctx.drawImage(img, 0,0);
-//  encoder.addFrame(ctx);
-//  URL.revokeObjectURL(url);
-//};
-
-//img.src = url;
-
 importScripts("LZWEncoder.js","NeuQuant.js","GIFEncoder.js","GenomesBlobs.js","Genome.js","BlobbyGen.js");//"path-data-polyfill.js"
 
 self.onmessage = function (msg) {
